9004a993a5d762739aecdfc4fec697f4b23a28c7
Daniel Santana <github@danielsantana.net>
2019-08-22 15:40:40 -0400

    cover 100% of the code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
 const pino = require('pino')
 
 const defaultOptions = {
-  level: process.env.LOG_LEVEL || 'info',
+  level: process.env.LOG_LEVEL,
   timestamp: !process.env.LOG_NODATE,
   base: null
 }
 
 const createLogger = (options = {}) => {
-  const pinoOptions = {...defaultOptions, ...options}
+  const pinoOptions = { ...defaultOptions, ...options }
   const mainLogr = pino(pinoOptions)
   mainLogr.createLogger = createLogger
 
   return mainLogr
 }
 
-module.exports = createLogger();
+module.exports = createLogger()
