d1bedf11a0e476bc457146155fe8297804f68ca5
Daniel Santana <daniel@everymundo.com>
2020-03-12 22:16:55 -0400

    add setRequestId feature

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 'use strict'
 
 const pino = require('pino')
+const flatstr = require('flatstr')
 
 const defaultOptions = {
   level: process.env.LOG_LEVEL || 'info',
-  timestamp: !process.env.LOG_NODATE,
+  get timestamp () {
+    if (process.env.LOG_NODATE) {
+      return false
+    }
+
+    if (process.env.LOG_DATE_FORMAT in pino.stdTimeFunctions) {
+      return pino.stdTimeFunctions[process.env.LOG_DATE_FORMAT]
+    }
+
+    return pino.stdTimeFunctions.epochTime
+  },
   useLevelLabels: true,
   base: null
+}
+
+// Default is this
+// ',"pid":2365,"hostname":"daniel-XPS-15-7590"'
+function setRequestId (RequestId) {
+  this[pino.symbols.chindingsSym] = flatstr(`,"RequestId":${JSON.stringify(RequestId)}`)
 }
 
 const createLogger = (options = {}) => {
   const pinoOptions = { ...defaultOptions, ...options }
   const mainLogr = pino(pinoOptions)
+
   mainLogr.createLogger = createLogger
+  mainLogr.setRequestId = setRequestId
+
+  mainLogr[pino.symbols.endSym] = '}\n'
 
   return mainLogr
 }
