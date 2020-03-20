7f6fdfad54a8cd4251acd784852585b69813be2b
Daniel Santana <daniel@everymundo.com>
2020-03-20 10:27:40 -0400

    remove lost console.log

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const defaultOptions = {
   useLevelLabels: true,
   base: null
 }
-console.log({defaultOptions})
+
 // Default is this
 // ',"pid":2365,"hostname":"daniel-XPS-15-7590"'
 function setRequestId (RequestId, substrStart = -12, substrEnd) {
